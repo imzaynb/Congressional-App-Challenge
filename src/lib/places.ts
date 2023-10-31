d30b22c35f3bb16aa227ab53887f2ee69725615e
Zayn Baig <zayn.baig@gmail.com>
2023-10-31 16:04:39 -0400

    one more

--- src/lib/places.ts
+++ src/lib/places.ts
@@ -1,85 +1,64 @@
 export const getBusinessDetails = (placeId: string, map: google.maps.Map) => {
-    const request = {
-        placeId: placeId,
-        fields: ["photos", "rating", "icon", "name", "type", "website", "formatted_address", "formatted_phone_number", "geometry"],
-    };
+  const request = {
+    placeId: placeId,
+    fields: ["photos", "rating", "icon", "name", "type", "website", "formatted_address", "formatted_phone_number", "geometry"],
+  };
 
-    const service = new google.maps.places.PlacesService(map);
-    service.getDetails(request, (place, status) => {
-        if (
-            status === google.maps.places.PlacesServiceStatus.OK &&
-            place && place.geometry && place.geometry.location
-        ) {
-            const photo = place.photos;
-            const rating = place.rating;
-            const icon = place.icon;
-            const name = place.name;
-            const type = place.types;
-            const website = place.website;
-            const address = place.formatted_address;
-            const phone = place.formatted_phone_number;
+  const service = new google.maps.places.PlacesService(map);
+  service.getDetails(request, (place, status) => {
+    if (
+      status === google.maps.places.PlacesServiceStatus.OK &&
+      place && place.geometry && place.geometry.location
+    ) {
+      const photo = place.photos;
+      const rating = place.rating;
+      const icon = place.icon;
+      const name = place.name;
+      const type = place.types;
+      const website = place.website;
+      const address = place.formatted_address;
+      const phone = place.formatted_phone_number;
 
-            if(!photo) {
-                return;
-            }
-            if(!rating) {
-                return;
-            }
-            if(!icon) {
-                return;
-            }
-            if(!name) {
-                return;
-            }
-            if(!type) {
-                return;
-            }
-            if(!website) {
-                return;
-            }
-            if(!address) {
-                return;
-            }
-            if(!phone) {
-                return;
-            }
+      if (!photo || !rating || !icon || !name || !type || !website || !address || !phone) {
+        return;
+      }
 
-            const photoLink = photo[0].getUrl();
-            const placeType = type[0];
+      const photoLink = photo[0].getUrl();
+      const placeType = type[0];
 
-            //Database add photoLink, rating, icon, name, placeType, website, address, phone
 
-        }
-    });
-
+    }
+  });
+}
 
 export interface Printwap {
-  photo: google.maps.places.PlacePhoto[] | undefined,
+  photo: string | undefined,
   rating: number | undefined,
   icon: string | undefined,
   name: string | undefined,
-  type: string[] | undefined,
+  type: string | undefined,
   website: string | undefined,
-
+  address: string | undefined,
+  phone_number: string | undefined
 }
 
-export const getPlaceIdFromQuery = async (query: string, map: google.maps.Map) => {
-    let res = "";
-    let service;
-    const request = {
-        query: query,
-        fields: ["place_id"],
-    };
+export const getPlaceIdFromQuery = async (query: string, map: google.maps.Map): Promise<string | null> => {
+  const request = {
+    query: query,
+    fields: ["place_id"],
+  };
+  let placeId = null;
+  let service = new google.maps.places.PlacesService(map);
+  await service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      console.log(results[0].place_id);
+      if (!results[0].place_id) {
+        return;
+      }
+      placeId = results[0].place_id;
+      return;
+    }
+  });
 
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, (results, status) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-            console.log(results[0].place_id);
-            if(!results[0].place_id) {
-                return;
-            }
-            res = results[0].place_id;
-            getBusinessDetails(res, map);
-        }
-    });
+  return placeId;
 }
